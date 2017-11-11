f43dec1ab5a69c4a887011d23783ca7985112c6b
Vladimir Surjaninov <vsurjaninov@gmail.com>
2017-11-11 12:40:14 +0300

    Change testing strategy

--- pmon/netlink_test.go
+++ pmon/netlink_test.go
@@ -2,33 +2,60 @@ package pmon
 
 import (
 	"fmt"
-	"time"
 	"testing"
+	"time"
 )
 
-func TestAck(t *testing.T) {
-	listener := &ProcListener{}
-	acks := make([]EventAck, 1)
+type testListener struct {
+	t        *testing.T
+	listener *ProcListener
+	done     chan bool
+	acks     []EventAck
+}
 
-	err := listener.Connect()
+func newTestListener(t *testing.T) *testListener {
+	tl := &testListener{
+		t:        t,
+		listener: &ProcListener{},
+		done:     make(chan bool, 1),
+	}
+
+	err := tl.listener.Connect()
 	if err != nil {
 		t.Fatal("Failed connect")
 	}
 
-	defer listener.Close()
-	go listener.ListenEvents()
-	time.Sleep(1)
+	go tl.listener.ListenEvents()
+	go func() {
+		for {
+			select {
+			case <-tl.done:
+				return
+			case <-tl.listener.Error:
+				t.Fatal("Error on recv")
+			case ev := <-tl.listener.EventAck:
+				fmt.Printf("%T no=%d\n", ev, ev.No)
+				tl.acks = append(tl.acks, *ev)
+			}
+		}
+	}()
 
-	select {
-	case err := <-listener.Error:
-		fmt.Println("error on receive: ", err)
-		t.Fatal("Error on recv")
-	case event := <-listener.EventAck:
-		fmt.Printf("%T no=%d\n", event, event.No)
-		acks = append(acks, *event)
-		listener.Close()
-		return
+	return tl
+}
+
+func (tl *testListener) close() {
+	pause := 100 * time.Millisecond
+	time.Sleep(pause)
+	tl.done <- true
+	tl.listener.Close()
+	time.Sleep(pause)
+}
+
+func TestAck(t *testing.T) {
+	tl := newTestListener(t)
+	tl.close()
+
+	if len(tl.acks) != 1 && tl.acks[0].No != 0 {
+		t.Errorf("Expected 1 ack event")
 	}
-
-	t.Errorf("No events received")
 }
